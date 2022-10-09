4c53cf72637cb27ce98f6f3779f1bb5ad3bed68e
Felix Galeano <felixlgq@gmail.com>
2022-10-09 20:16:32 -0300

    head do site

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,12 +1,18 @@
 import { Box } from "@mui/material";
+import Head from "next/head";
 import Footer from "./Footer";
 import Topbar from "./Topbar";
-
 const Layout: React.FC<{
   children: React.ReactNode;
 }> = ({ children }) => {
   return (
     <>
+      <Head>
+        <link rel="favicon" href="/favicon.ico" />
+        <title>
+          MASTER - Meteorologia Aplicada a Sistemas de Tempo Regionais
+        </title>
+      </Head>
       <Topbar />
       <Box component="main" sx={{ flexGrow: 1, p: 3, pt: 10, display: "flex" }}>
         {children}
